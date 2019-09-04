23f445f02bf46e686480b3cf1dc4d3f15f2e0e29
shehbazjafri <galaxytwirls@gmail.com>
2019-09-04 11:36:37 +0530

    Added routing and test script

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import BarChart from "./components/BarChart";
 import "./App.css";
 
-function App() {
+function App(props) {
+  const openVis = vis => {
+    props.history.push(`/${vis}`);
+  };
+
   return (
     <div className="App">
-      <header>D3 Visualizations</header>
-      <div className="container">
-        <button className="bar button">Bar Chart</button>
-      </div>
+      <Route
+        exact
+        path="/"
+        render={props => (
+          <React.Fragment>
+            <header>D3 Visualizations</header>
+            <div className="container">
+              <button
+                className="bar button"
+                onClick={() => openVis("bar-chart")}
+              >
+                Bar Chart
+              </button>
+            </div>
+          </React.Fragment>
+        )}
+      />
+      <Route path="/bar-chart" render={props => <BarChart />} />
     </div>
   );
 }
 
-export default App;
+export default () => (
+  <Router>
+    <Route component={App} />
+  </Router>
+);
