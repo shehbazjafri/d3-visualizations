486f10de9727cecd4d3a3171e57839be0a7a1e8c
shehbazjafri <galaxytwirls@gmail.com>
2019-09-26 15:48:29 +0530

    Fixed y axis labels on heat map and added comments

--- src/components/HeatMap.js
+++ src/components/HeatMap.js
@@ -3,34 +3,67 @@ import * as d3 from "d3";
 
 export default function HeatMap() {
   const drawChart = () => {
-    const padding = 200;
-    const svgWidth = 1000;
-    const svgHeight = 600;
-    const width = svgWidth - padding;
-    const height = svgHeight - padding;
+    const padding = 200,
+      svgWidth = 1000,
+      svgHeight = 600,
+      width = svgWidth - padding,
+      height = svgHeight - padding,
+      months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
 
+    // Create svg canvas
     const svg = d3
       .select("#heatmap")
       .append("svg")
       .attr("width", svgWidth)
       .attr("height", svgHeight);
 
-    const xScale = d3.scaleLinear().range([0, width]);
+    // Append group container to svg that will contain axes and graph
+    const g = svg.append("g").attr("transform", `translate(${100},${100})`);
 
-    const yScale = d3.scaleLinear().range([0, height]);
+    // Create x and y scales
+    const xScale = d3.scaleTime().range([0, width]);
+    const yScale = d3.scaleBand().range([0, height]);
 
+    // Create x and y axis that will be called on group element later
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
 
-    const g = svg.append("g").attr("transform", `translate(${100},${100})`);
+    // const colorScale = d3.scale.quantize().range(color);
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
     d3.json(
       "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json"
     ).then(function(data) {
-      const years = data.monthlyVariance.map(d => d.year);
-      xScale.domain([d3.min(years) - 1, d3.max(years) + 1]);
+      data = data.monthlyVariance;
 
-      yScale.domain([1, 12]);
+      const timeFormat = d3.timeFormat("%Y");
+
+      data.forEach(function(d) {
+        d.month = months[d.month - 1];
+        // d.year = timeFormat(d.year);
+      });
+
+      const years = data.map(d => d.year);
+      xScale.domain(
+        d3.extent(data, function(d) {
+          return d.year;
+        })
+      );
+
+      yScale.domain(months);
 
       //   title
       svg
@@ -62,7 +95,7 @@ export default function HeatMap() {
         .call(yAxis);
 
       g.selectAll(".bar")
-        .data(data.data)
+        .data(data)
         .enter()
         .append("rect")
         .attr("class", "cell");
