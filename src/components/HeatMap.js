5d793e69d4a27def0c89ccfdcaf1fbf860043a75
shehbazjafri <galaxytwirls@gmail.com>
2019-09-27 23:59:08 +0530

    Added inital heading and comments on heatmap

--- src/components/HeatMap.js
+++ src/components/HeatMap.js
@@ -3,25 +3,13 @@ import * as d3 from "d3";
 
 export default function HeatMap() {
   const drawChart = () => {
-    const padding = 200,
-      svgWidth = 1000,
-      svgHeight = 600,
-      width = svgWidth - padding,
-      height = svgHeight - padding,
-      months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-      ];
+    // Settings for svg canvas size
+    const padding = 200;
+    const svgWidth = 900;
+    const svgHeight = 600;
+    // Actual width and height of the graph spaced using padding
+    const width = svgWidth - padding;
+    const height = svgHeight - padding;
 
     // Create svg canvas
     const svg = d3
@@ -30,75 +18,40 @@ export default function HeatMap() {
       .attr("width", svgWidth)
       .attr("height", svgHeight);
 
-    // Append group container to svg that will contain axes and graph
+    // Create group container on svg that will contain axes and graph
     const g = svg.append("g").attr("transform", `translate(${100},${100})`);
 
-    // Create x and y scales
-    const xScale = d3.scaleTime().range([0, width]);
-    const yScale = d3.scaleBand().range([0, height]);
+    // Get data from api
+    const url =
+      "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 
-    // Create x and y axis that will be called on group element later
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
+    d3.json(url).then(function(data) {
+      // Format data
 
-    // const colorScale = d3.scale.quantize().range(color);
-    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-    d3.json(
-      "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json"
-    ).then(function(data) {
-      data = data.monthlyVariance;
-
-      const timeFormat = d3.timeFormat("%Y");
-
-      data.forEach(function(d) {
-        d.month = months[d.month - 1];
-        // d.year = timeFormat(d.year);
-      });
-
-      const years = data.map(d => d.year);
-      xScale.domain(
-        d3.extent(data, function(d) {
-          return d.year;
-        })
-      );
-
-      yScale.domain(months);
-
-      //   title
-      svg
-        .append("text")
+      // Add heading to svg
+      const heading = svg.append("heading");
+      heading
+        .append("h1")
         .attr("id", "title")
-        .attr("x", width / 2)
-        .attr("y", 0 - padding / 2)
-        .attr("text-anchor", "middle")
-        .style("font-size", "30px")
         .text("Monthly Global Land-Surface Temperature");
-
-      //subtitle
-      svg
-        .append("text")
+      heading
+        .append("h3")
         .attr("id", "description")
-        .attr("x", width / 2)
-        .attr("y", 0 - padding / 2 + 25)
-        .attr("text-anchor", "middle")
-        .style("font-size", "20px")
-        .text("1753 - 2015: base temperature 8.66℃");
-
-      g.append("g")
-        .attr("id", "x-axis")
-        .attr("transform", `translate(0,${height})`)
-        .call(xAxis);
-
-      g.append("g")
-        .attr("id", "y-axis")
-        .call(yAxis);
-
-      g.selectAll(".bar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("class", "cell");
+        .html(
+          data.monthlyVariance[0].year +
+            " - " +
+            data.monthlyVariance[data.monthlyVariance.length - 1].year +
+            ": base temperature " +
+            data.baseTemperature +
+            "&#8451;"
+        );
+      // Create tooltip with d3.tip
+      // Create x and y scales
+      // Create x and y axis
+      // Call x axis
+      // Call y axis
+      // Create and add legend
+      // Create the heat map with rects
     });
   };
 
