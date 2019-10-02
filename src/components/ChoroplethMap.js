a67af7abdce4ce8b30748449347bb5ac10b26a0b
shehbazjafri <galaxytwirls@gmail.com>
2019-10-02 14:21:07 +0530

    Init choropleth

---
+++ src/components/ChoroplethMap.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ChoroplethMap() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Choropleth Map</h1>
+      </header>
+      <div id="choropleth"></div>
+    </div>
+  );
+}
